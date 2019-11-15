07be9a0f9b658fb88ca4281af25051e70e11932b
sforman2000 <formanforyou@gmail.com>
2019-11-14 19:38:06 -0800

    Nina's feature changes

--- runner.py
+++ runner.py
@@ -3,6 +3,7 @@ import sys
 import pickle
 import algorithms
 import visualizer
+import features
 
 class Runner():
 
@@ -17,22 +18,37 @@ class Runner():
         (X_test, Y_test) = pickle.load(open("test.pickle", 'rb'))
         # Extract features
         if self.featureExt:
-            (X_train, X_test) = [self.featureExt(X) for X in (X_train, X_test)]
+            print(self.featureExt)
+            if self.featureExt.lower() == "pca":
+                (X_train, X_test) = [features.doPCA(X) for X in (X_train, X_test)]
+            elif self.featureExt.lower() == "ica":
+                (X_train, X_test) = [features.doICA(X) for X in (X_train, X_test)]
+            elif self.featureExt.lower() == "lle":
+                (X_train, X_test) = [features.doLLE(X) for X in (X_train, X_test)]
+            elif self.featureExt.lower() == "tsne":
+                (X_train, X_test) = [features.doTSNE(X) for X in (X_train, X_test)]
+
         # Run algorithm
         output = self.algo(X_train,Y_train, X_test, Y_test)
         # Visualize
         if self.visualize:
            self.visualize(output, Y_test)
+        #return output
 
 if __name__ == '__main__':
-    assert(len(sys.argv) == 2)
+    assert(len(sys.argv) <= 3)
     mode = sys.argv[1]
+    feature = None
+    if len(sys.argv) == 3:
+        feature = sys.argv[2]
+
     linear_names = ["linear", "linear regression", "linear_regression, baseline"]
     nn_names = ["nn", "nn_regression", "nn regression", "neural net", "neural_net"]
+
     if mode.lower() in linear_names:
-        runner = Runner(algorithms.linear_regression, None, visualizer.visualize_linear_regression)
+        runner = Runner(algorithms.linear_regression, feature, visualizer.visualize_linear_regression)
     elif mode.lower() in nn_names:
-        runner = Runner(algorithms.NN_regression, None, visualizer.visualize_NN_regression)
+        runner = Runner(algorithms.NN_regression, feature, visualizer.visualize_NN_regression)
     else:
         raise(f"IDK what the mode {mode} is. Pls change ur input parameter")
     runner.run()
